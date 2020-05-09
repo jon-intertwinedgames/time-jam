960327b137677cf9299c58f42abdefc26e59c912
LeoTheLegion <Halelt@live.com>
2020-05-08 22:47:58 -0400

    Added Simple scene management. Created Scenes Start and end

---
+++ Assets/Scripts/SceneManagement/SceneManagement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    static SceneManagement INSTANCE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        INSTANCE = this;
+    }
+
+    public static void LoadScene( int buildindex)
+    {
+        SceneManager.LoadScene(buildindex);
+    }
+
+    public static void LoadNextScene()
+    {
+        LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public static void LoadRestartCurrentScene()
+    {
+        LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/SceneManagement/SceneManagerWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerWrapper : MonoBehaviour
+{
+    public void LoadScene(int buildindex)
+    {
+        SceneManagement.LoadScene(buildindex);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManagement.LoadNextScene();
+    }
+
+    public void LoadRestartCurrentScene()
+    {
+        SceneManagement.LoadRestartCurrentScene();
+    }
+}
