bb060e3881c539ddfb9feb0e277436f673c31c70
LeoTheLegion <Halelt@live.com>
2020-05-08 23:54:34 -0400

    Added Spawner. Create gameplay scene. Added End screen results. Added results to player HUD.

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    [SerializeField]
+    int numOfEnemiesToKillToWin = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GlobalVars.EnemiesKilled = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GlobalVars.EnemiesKilled >= numOfEnemiesToKillToWin)
+            GetComponent<SceneManagerWrapper>().LoadNextScene();
+    }
+}
---
+++ Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Use this class for statistical reasons 
+/// </summary>
+public static class GlobalVars
+{
+    public static int EnemiesKilled = 0;
+}
