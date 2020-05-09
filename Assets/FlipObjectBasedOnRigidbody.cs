2cc0a07148d5561af6976befc0c10df247999d6e
LeoTheLegion <Halelt@live.com>
2020-05-09 00:31:23 -0400

    Updated Drifter with updated sprite

---
+++ Assets/FlipObjectBasedOnRigidbody.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipObjectBasedOnRigidbody : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb)
+            if (rb.velocity.x > 0)
+                this.transform.localScale = new Vector3(1, this.transform.localScale.y, this.transform.localScale.z);
+            else if(rb.velocity.x < 0)
+                this.transform.localScale = new Vector3(-1, this.transform.localScale.y, this.transform.localScale.z);
+            else
+            {
+                //do nothing
+            }
+    }
+}
