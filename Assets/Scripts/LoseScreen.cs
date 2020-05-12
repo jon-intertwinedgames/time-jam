5fe3cabcfdc024f54c37871c1f9b1f1a4141bcb9
LeoTheLegion <Halelt@live.com>
2020-05-11 21:59:47 -0400

    Created a quick fader. OnGameOver event has been added to Gamemaster

---
+++ Assets/Scripts/LoseScreen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Fader))]
+public class LoseScreen : MonoBehaviour
+{
+    Fader fader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fader = GetComponent<Fader>();
+        GameObject.FindObjectOfType<GameMaster>().GameOver += Lose;
+    }
+
+    private void Lose(object sender, EventArgs e)
+    {
+        fader.FadeOut(1f);
+    }
+}
