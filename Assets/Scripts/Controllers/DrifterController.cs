c150f7891ea1cfbc9d0bf6a498a1a17e57a206f6
LeoTheLegion <Halelt@live.com>
2020-05-19 22:09:49 -0400

    Removed random print log

--- Assets/Scripts/Controllers/DrifterController.cs
+++ Assets/Scripts/Controllers/DrifterController.cs
@@ -78,7 +78,6 @@ public class DrifterController : MonoBehaviour
             ShootAtPlayer();
 
         particleSystemEmission.rateOverTimeMultiplier = 50f * (time / firerate);//<-- LOOK HERE Magic number :D
-        print("");
     }
 
     private void FixedUpdate()
