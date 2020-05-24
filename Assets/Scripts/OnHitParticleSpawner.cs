98c06ff24811a7fd75271535dd7f81636eb04980
LeoTheLegion <Halelt@live.com>
2020-05-24 00:32:43 -0400

    Add the Dark On-Hit Particle effect positioned on the player when the player gets hit by the beam

---
+++ Assets/Scripts/OnHitParticleSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHitParticleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private SpawnParticlebyTag[] spawnParticlebyTag;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach (var item in spawnParticlebyTag)
+        {
+            if(item.tag == collision.tag)
+            {
+                //ContactPoint2D[] points = new ContactPoint2D[10];
+                //var length = GetComponent<Collider2D>().GetContacts(points);
+
+                //for (int i = 0; i < length; i++)
+                //{
+                //    GameObject.Instantiate(item.prefab, points[i].point, Quaternion.identity);
+                //}
+                var p = GameObject.Instantiate(item.prefab, collision.gameObject.transform.position, Quaternion.identity);
+                Destroy(p.gameObject,5f);
+            }
+        }
+    }
+
+    [System.Serializable]
+    private struct SpawnParticlebyTag {
+        public string tag;
+        public ParticleSystem prefab;
+    }
+
+}
