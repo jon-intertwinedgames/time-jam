a97ba4b5d1a6f397781b2c43eaf0d1c062ded519
Charles Fernandez <charliefernandez777@gmail.com>
2020-05-31 20:45:15 -0400

    Pause Menu with bugs when loading new scene

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -3,12 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameState
+{
+    Playing,
+    Paused
+}
 
 public class GameMaster : MonoBehaviour
 {
     [SerializeField]
     public int numOfEnemiesToKillToWin = 2;
 
+    private CanvasGroup pauseCanvasGroup;
+    private static GameState state;
+    public static GameState State { get => state; }
 
     [SerializeField]
     TextMeshProUGUI howmanyToKilled_text = null;
@@ -16,7 +26,12 @@ public class GameMaster : MonoBehaviour
     public event EventHandler GameOver;
     private bool isGameOver = false;
 
-    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += OnNewLevelLoaded;
+    }
+
     void Start()
     {
         GlobalVars.EnemiesKilled = 0;
@@ -30,6 +45,11 @@ public class GameMaster : MonoBehaviour
 
         CheckforWin();
         CheckforLose();
+
+        if(Input.GetButtonDown("Pause"))
+        {
+            TogglePause();
+        }
     }
 
     private void CheckforLose()
@@ -65,6 +85,51 @@ public class GameMaster : MonoBehaviour
         if (handler != null)
         {
             handler(this, e);
+        }
+    }
+
+    public void TogglePause()
+    {        
+        if (pauseCanvasGroup)
+        {
+            switch (state)
+            {
+                case GameState.Playing:
+                    state = GameState.Paused;
+                    Time.timeScale = 0 ;
+                    pauseCanvasGroup.alpha = 1;
+                    pauseCanvasGroup.interactable = true;                    
+                    break;
+                case GameState.Paused:
+                    state = GameState.Playing;
+                    Time.timeScale = 1;
+                    pauseCanvasGroup.alpha = 0;
+                    pauseCanvasGroup.interactable = false;
+
+                    print("Made it     " + state + " " + Time.timeScale);
+                    break;
+            }   
+        }
+    }
+
+    private void OnNewLevelLoaded(Scene newScene, LoadSceneMode sceneMode)
+    {
+        GameObject pauseScreen = GameObject.FindGameObjectWithTag("Pause Screen");
+        pauseCanvasGroup = pauseScreen?.GetComponent<CanvasGroup>() ?? null;
+
+        if (pauseCanvasGroup)
+        {
+            state = GameState.Paused;
+            TogglePause();
+        }
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus == false)
+        {
+            state = GameState.Playing;
+            TogglePause();
         }
     }
 }
